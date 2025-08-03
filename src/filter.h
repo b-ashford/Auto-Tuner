88e7b11cb016a0314da9a6d0c6cfcfb77c1ef68c
Bronston Ashford <bronston.a@icloud.com>
2025-08-04 06:50:32 +1000

    bandpass filter coeffs determined

--- src/filter.h
+++ src/filter.h
@@ -3,22 +3,22 @@
 
 #include "arm_math.h"
 
-arm_biquad_casd_df1_inst_f32 iir_settings;
-float iir_state[4];
-/*
-static float iir_taps[5] = {
-    0.997987115675119,
-    -1.995974231350238,
-    0.997987115675119,
-    1.995970179642828,
-    -0.995978283057647};
-*/
-static float iir_taps[5] = {
-    0.997987115675119,
-    -1.995974231350238,
-    0.997987115675119,
-    1.995970179642828,
-    -0.995978283057647};
+#define NUM_IIR_STAGES 3
 
+arm_biquad_casd_df1_inst_f32 iir_settings;
+float iir_state[4 * NUM_IIR_STAGES];
+
+
+// CMSIS-DSP coefficient format: [b0, b1, b2, -a1, -a2] per stage
+static float iir_taps[] = {
+    // Stage 1: HP Filter (80Hz)
+    0.95653708, -1.91307417, 0.95653708, 1.91118480, -0.91496354,
+
+    // Stage 2: HP Filter (80Hz)
+    0.95653708, -1.91307417, 0.95653708, 1.91118480, -0.91496354,
+
+    // Stage 3: Lowpass Filter (1kHz)
+    0.125, 0.25, 0.125, 0.0, 0.0};
 
 #endif /* FILTER_H */
+
