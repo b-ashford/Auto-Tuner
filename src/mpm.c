ebfaae94fff84e256ec079b23d548ca4ae908907
Bronston Ashford <bronston.a@icloud.com>
2025-08-04 10:19:54 +1000

    added dependency injection to decouple from main application

--- src/mpm.c
+++ src/mpm.c
@@ -1,84 +1,120 @@
-
-
-/*
+/**
+ * @file mpm.c
+ * @brief McLeod Pitch Method Implementation
  *
- *  McLeod Pitch Method
- *  A smarter way to find pitch: https://www.cs.otago.ac.nz/students/postgrads/tartini/papers/A_Smarter_Way_to_Find_Pitch.pdf
+ * A smarter way to find pitch using normalized auto-correlation
+ * and parabolic interpolation for increased accuracy.
  *
- *  Uses a normalised auto correlation and parabolic intperpolation for increased accuracy.
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <math.h>
 #include "mpm.h"
-#include "autotune.h"
 
-static float32_t mpm_reserved_memory[2 * BLOCK_SIZE] = {0};
+//==============================================================================
+// Private Constants & Memory
+//==============================================================================
+static float32_t mpm_reserved_memory[MPM_MAX_SIGNAL_SIZE] = {0};
 
-void mpm_sum_f32(float32_t *pSrc, uint16_t scrLen, float32_t *pRes)
+//==============================================================================
+// Private Function Declarations
+//==============================================================================
+static void mpm_sum_f32(float32_t *pSrc, uint16_t scrLen, float32_t *pRes);
+static uint16_t mpm_find_peak_bin_f32(float32_t *pSrc, int len, float threshold);
+static void mpm_NSDF_f32(float32_t *src, float32_t **dst, int len);
+static float mpm_parabolic_interpolation_f32(
+	uint16_t x_pos,
+	float32_t a,
+	float32_t b,
+	float32_t c);
+
+//==============================================================================
+// Public API Implementation
+//==============================================================================
+
+float mpm_get_pitch_f32(
+	float32_t *signal,
+	int signal_len,
+	int sample_rate,
+	float threshold)
+{
+	float32_t *p_ncorr;
+	mpm_NSDF_f32(signal, &p_ncorr, signal_len);
+	uint16_t tau = mpm_find_peak_bin_f32(p_ncorr, signal_len, threshold);
+
+	if (tau > signal_len - 2)
+		tau = signal_len - 2;
+
+	uint16_t xp = tau;
+	float32_t a = p_ncorr[tau - 1];
+	float32_t b = p_ncorr[tau];
+	float32_t c = p_ncorr[tau + 1];
+
+	float delta_tau = mpm_parabolic_interpolation_f32(xp, a, b, c);
+
+	float pitch = sample_rate * 1.0f / delta_tau;
+	return pitch;
+}
+
+//==============================================================================
+// Private Helper Functions
+//==============================================================================
+static void mpm_sum_f32(float32_t *signal, uint16_t scrLen, float32_t *pRes)
 {
 	*pRes = 0;
 	for (uint16_t i = 0; i < scrLen; i++)
 	{
-		*pRes += *pSrc;
-		pSrc++;
+		*pRes += *signal;
+		signal++;
 	}
 }
 
-void mpm_find_peak_f32(float32_t *pSrc, uint16_t *tau)
+static uint16_t mpm_find_peak_bin_f32(float32_t *signal, int len, float threshold)
 {
-	uint16_t flag = 0;
-	uint16_t valid_peak_flag = 0;
+	uint16_t past_zero = 0;
+	uint16_t valid_peak_past_zero = 0;
 	float32_t peak_value = 0;
-
-	for (uint16_t i = 0; i < BLOCK_SIZE; i++)
+	uint16_t tau = 1;
+	for (uint16_t i = 0; i < len; i++)
 	{
+		if (past_zero == 0 && *signal < 0)
+			past_zero = 1;
 
-		if (flag == 0 && *pSrc < 0)
+		if (past_zero == 1)
 		{
-			flag = 1;
-		}
-		if (flag == 1)
-		{
-			if (*pSrc > peak_value && *pSrc > PEAK_THRESHOLD)
+			if (*signal > peak_value && *signal > threshold)
 			{
-				peak_value = *pSrc;
-				*tau = i;
-				valid_peak_flag = 1;
+				peak_value = *signal;
+				tau = i;
+				valid_peak_past_zero = 1;
 			}
-			else if (valid_peak_flag == 1)
-			{
-				return;
-			}
+			else if (valid_peak_past_zero == 1)
+				return tau;
 		}
-		pSrc++;
+		signal++;
 	}
+	return tau;
 }
 
-void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst)
+static void mpm_NSDF_f32(float32_t *src, float32_t **dst, int len)
 {
 
 	float32_t *xcorr = &mpm_reserved_memory[1];
 
-	arm_correlate_f32(&pSrc[0], BLOCK_SIZE, &pSrc[0], BLOCK_SIZE, xcorr);
+	arm_correlate_f32(&src[0], len, &src[0], len, xcorr);
 
-	float32_t *r = &xcorr[BLOCK_SIZE - 1];
-	*pDst = r;
+	float32_t *r = &xcorr[len - 1];
+	*dst = r;
 
 	float32_t *xs = &mpm_reserved_memory[0];
 	float32_t *p_xs1 = &xs[0];
-	float32_t *p_xs2 = &xs[BLOCK_SIZE - 1];
+	float32_t *p_xs2 = &xs[len - 1];
 	float32_t xs1, xs2;
 
-	arm_mult_f32(&pSrc[0], &pSrc[0], &xs[0], BLOCK_SIZE);
-	mpm_sum_f32(&xs[0], BLOCK_SIZE, &xs1);
+	arm_mult_f32(&src[0], &src[0], &xs[0], len);
+	mpm_sum_f32(&xs[0], len, &xs1);
 	xs2 = xs1;
 
-	for (uint16_t tau = 0; tau < BLOCK_SIZE; tau++)
+	for (uint16_t tau = 0; tau < len / 2; tau++)
 	{
-
 		*r = 2 * (*r) / (xs1 + xs2);
 
 		xs1 = xs1 - (*p_xs1);
@@ -90,39 +126,17 @@ void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst)
 	}
 }
 
-void mpm_parabolic_interpolation_f32(uint16_t x_pos, float32_t a, float32_t b, float32_t c, float32_t *delta_tau)
+static float mpm_parabolic_interpolation_f32(
+	uint16_t x_pos,
+	float32_t a,
+	float32_t b,
+	float32_t c)
 {
 	a = 20 * log10(a);
 	b = 20 * log10(b);
 	c = 20 * log10(c);
 
-	float32_t delta_pos = 0.5 * (a - c) / (1 - 2.0 * b + c);
+	float delta_pos = 0.5 * (a - c) / (1 - 2.0 * b + c);
 
-	*delta_tau = x_pos + delta_pos;
-}
-
-float mpm_get_pitch(float32_t *pData)
-{
-
-	float32_t *p_ncorr;
-
-	mpm_NSDF_f32(pData, &p_ncorr);
-	uint16_t tau = 1;
-	mpm_find_peak_f32(p_ncorr, &tau);
-
-	if (tau > BLOCK_SIZE - 2)
-	{
-		tau = BLOCK_SIZE - 2;
-	}
-
-	uint16_t xp = tau;
-	float32_t a = p_ncorr[tau - 1];
-	float32_t b = p_ncorr[tau];
-	float32_t c = p_ncorr[tau + 1];
-
-	float32_t delta_tau = 0;
-	mpm_parabolic_interpolation_f32(xp, a, b, c, &delta_tau);
-
-	float pitch = FS * 1.0f / delta_tau;
-	return pitch;
+	return x_pos + delta_pos;
 }
--- src/mpm.h
+++ src/mpm.h
@@ -1,8 +1,12 @@
-/*
- * mpm.h
+/**
+ * @file mpm.h
+ * @brief McLeod Pitch Method - High-accuracy pitch detection
  *
- *  Created on: 26 Jul. 2022
- *      Author: brono
+ * Implementation of the McLeod Pitch Method for robust fundamental
+ * frequency estimation using normalised square difference function.
+ *
+ * @author Bronston Ashford
+ * @date 26 Jul. 2022
  */
 
 #ifndef MPM_H
@@ -10,20 +14,37 @@
 
 #include "arm_math.h"
 
-#define FS 8000
-#define BLOCK_SIZE 1024
-#define PEAK_THRESHOLD 0.5
+//==============================================================================
+// Configuration Constants
+//==============================================================================
 
-float mpm_get_pitch(float32_t *pData);
-void mpm_sum_f32(float32_t *pSrc, uint16_t scrLen, float32_t *pRes);
-void mpm_find_peak_f32(float32_t *pSrc, uint16_t *tau);
-void mpm_NSDF_f32(float32_t *pSrc, float32_t **pDst);
-void mpm_parabolic_interpolation_f32(
-    uint16_t x_pos,
-    float32_t a,
-    float32_t b,
-    float32_t c,
-    float32_t *delta_tau);
+/** @brief Maximum signal length supported by MPM algorithm
+ *  @note Must accommodate 2x signal length for internal correlation buffer
+ */
 
+#define MPM_MAX_SIGNAL_SIZE 2048 // Needs to fit 2*signal length at minimum
+
+//==============================================================================
+// Public API
+//==============================================================================
+
+/**
+ * @brief Extract fundamental frequency from audio signal using McLeod Pitch Method
+ *
+ * @param signal      Input audio signal buffer
+ * @param signal_len  Length of input signal (must be <= MPM_MAX_SIGNAL_SIZE/2)
+ * @param sample_rate Sample rate of input signal in Hz
+ * @param threshold   Peak detection threshold (typically 0.3 - 0.9)
+ *
+ * @return Detected fundamental frequency in Hz, or 0.0f if no pitch found
+ *
+ * @note Function uses internal static memory for correlation calculations
+ * @note Higher threshold values require stronger, clearer pitch signals
+ */
+float mpm_get_pitch_f32(
+    float32_t *signal,
+    int signal_len,
+    int sample_rate,
+    float threshold);
 
 #endif
