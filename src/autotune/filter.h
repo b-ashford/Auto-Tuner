bf7d335601e6291b944b052b25ba45cd284a19d6
Bronston Ashford <bronston.a@icloud.com>
2025-08-16 17:28:10 +1000

    using tighter filter

--- src/autotune/filter.h
+++ src/autotune/filter.h
@@ -3,8 +3,7 @@
 
 #include "arm_math.h"
 
-#define NUM_IIR_STAGES 3
-
+#define NUM_IIR_STAGES 4
 
 extern float32_t iir_taps_f32[];
 extern q15_t iir_taps_q15[];
@@ -13,9 +12,13 @@ extern q15_t iir_state_q15[4 * NUM_IIR_STAGES];
 extern arm_biquad_casd_df1_inst_f32 iir_settings_f32;
 extern arm_biquad_casd_df1_inst_q15 iir_settings_q15;
 
+
+
+
+
 void init_iir_filter_f32(void);
 void init_iir_filter_q15(void);
-void filter_and_remove_dc_f32(uint16_t *src, float32_t *guitar_signal, int len);
+void filter_and_remove_dc_f32(const uint16_t *src, float32_t *y, int len);
 void filter_and_remove_dc_q15(uint16_t *src, q15_t *guitar_signal, int len);
 void one_sided_autocorr_f32(const float32_t *src, float32_t *xcorr, uint32_t len);
 
