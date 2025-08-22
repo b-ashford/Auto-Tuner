13565d46bb8008241cb555eb1a5fadf9f7d3beb8
Bronston Ashford <bronston.a@icloud.com>
2025-08-22 20:06:52 +1000

    added utils

--- cmake/globals.cmake
+++ cmake/globals.cmake
@@ -10,6 +10,7 @@ set(DIR_CMAKE ${DIR_ROOT}/cmake)
 # Application
 set(DIR_APP ${DIR_ROOT}/app)
 set(DIR_APP_AUTOTUNE ${DIR_APP}/autotune)
+#set(DIR_APP_UTILITIES ${DIR_APP}/utilities)
 
 # Drivers
 set(DIR_HW ${DIR_ROOT}/hw)
@@ -26,7 +27,7 @@ set(DIR_L4HAL ${DIR_CUBEL4}/STM32L4xx_HAL_Driver)
 #--------------------------------------#
 #              TOOLCHAINS              #
 #--------------------------------------#
-set(TOOLCHAIN_ARM_NONE_EABI "${DIR_CMAKE}/arm-none-eabi.cmake")
+set(TOOLCHAIN_ARM_NONE_EABI "${DIR_CMAKE}/arm-none-eabi-toolchain.cmake")
 
 #--------------------------------------#
 #           SUPPORTED BOARDS           #
